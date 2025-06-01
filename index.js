12618b3165019300c0af3ccf16c67eedd08ce673
seragithantsha <tiishetsoseragi@gmail.com>
2025-06-01 20:57:01 +0200

    update

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ const swiper = new Swiper(".swiper-container", {
 		clickable: true,
 	},
 });
+
+// Select elements
+const thumbnails = document.querySelectorAll(".thumbnails img");
+const popup = document.getElementById("popup");
+const popupImg = document.getElementById("popup-img");
+const closeBtn = document.getElementById("close-btn");
+
+// Add click event to each thumbnail
+thumbnails.forEach((thumbnail) => {
+	thumbnail.addEventListener("click", () => {
+		const src = thumbnail.getAttribute("data-src"); // Get the larger image source
+		popupImg.src = src; // Set the pop-up image source
+		popup.style.display = "flex"; // Show the pop-up
+	});
+});
+
+// Close the pop-up when the close button is clicked
+closeBtn.addEventListener("click", () => {
+	popup.style.display = "none";
+});
+
+// Close the pop-up when clicking outside the image
+popup.addEventListener("click", (e) => {
+	if (e.target === popup) {
+		popup.style.display = "none";
+	}
+});
